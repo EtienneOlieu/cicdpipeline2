b7cc8e2e5bf2b791d57a7d6fc6f5c0237d603cb8
Horrid Entity <etienne.olieu@gmail.com>
2022-09-05 13:04:33 +0200

    addded sout line

--- src/main/java/com/example/cicdpipeline/CicdpipelineApplication.java
+++ src/main/java/com/example/cicdpipeline/CicdpipelineApplication.java
@@ -9,6 +9,7 @@ public class CicdpipelineApplication {
     public static void main(String[] args) {
         SpringApplication.run(CicdpipelineApplication.class, args);
         System.out.println("hello world");
+        System.out.println("My pants are super big");
     }
 
 }
